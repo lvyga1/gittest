a5146378fec7960a13fb36fef69e9ae6e50aa363
lvxin <775358128@qq.com>
2019-05-29 14:31:29 +0800

    测试GIT

--- src/main/java/com/montnets/git/GittestApplication.java
+++ src/main/java/com/montnets/git/GittestApplication.java
@@ -2,7 +2,7 @@ package com.montnets.git;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//spring boot 开始啦
 @SpringBootApplication
 public class GittestApplication {
 
