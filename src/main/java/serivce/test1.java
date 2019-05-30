204200e4bcf40576027fbc0c1d69e5d25c36dc06
tanlq123 <1377397194@qq.com>
2019-05-30 11:31:06 +0800

    测试

--- src/main/java/serivce/test1.java
+++ src/main/java/serivce/test1.java
@@ -25,6 +25,7 @@ public class test1 {
 
     public static void main(String[] args) {
         System.out.println(111);
+        System.out.println(111);
         /***
          *                  ___====-_  _-====___
          *            _--^^^#####//      \\#####^^^--_
